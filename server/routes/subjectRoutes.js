b78549cf1f9cdd2a2b5de4b31790e94a27fd88dd
Sumaiya Shah <108864503+sumaiyashah27@users.noreply.github.com>
2024-12-24 10:54:31 +0530

    Update subjectRoutes.js

--- server/routes/subjectRoutes.js
+++ server/routes/subjectRoutes.js
@@ -190,7 +190,7 @@ router.get("/questions", async (req, res) => {
 
 
 // Add a new question for a subject (with image upload)
-router.post('/subjects/:subjectId/questions/add', async (req, res) => {
+router.post('api/subjects/:subjectId/questions/add', async (req, res) => {
   try {
     // Validate incoming data
     validateQuestionData(req.body);
